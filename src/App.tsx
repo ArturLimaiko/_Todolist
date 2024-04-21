2186fd59aaf0161df8e7c51241bb60171f8f3a93
Artur <alimayko91@gmail.com>
2024-04-21 16:01:50 +0300

    lesson 07- add Reducer & create tasksReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
@@ -7,6 +7,7 @@ import {ButtonAppButton} from "./ButtonAppBar/ButtonAppButton";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import {addTaskAC, removeTaskAC, changeStatusAC, tasksReducer, updateTaskTitleAC} from "./Reducer/tasksReducer";
 
 export type FilterValuesType = "all" | "active" | "completed";
 
@@ -16,8 +17,8 @@ export type TodoListType = {
     filter: FilterValuesType
 }
 
-type TaskStateType = {
-    [key: string]: Array<TaskType>
+export type TaskStateType = {
+    [key: string]: TaskType[]
 }
 
 function App() {
@@ -29,7 +30,7 @@ function App() {
         {id: todolistID2, title: "What to bye", filter: 'active'},
     ])
 
-    let [tasks, setTasks] = useState<TaskStateType>({
+    let [tasks, dispatchTasks] = useReducer(tasksReducer,{
         [todolistID1]: [
             {id: v1(), title: "HTML&CSS", isDone: true},
             {id: v1(), title: "JS", isDone: true},
@@ -47,32 +48,45 @@ function App() {
         ]
     });
 
-    //Функция удаления таски
+    //Функция удаления таски ON REDUCER
     function removeTask(todolistID: string, taskID: string) {
         // копируем все таски {...tasks , потом взяли ключ [todolistID] , далее тут указываем какие именно таски - 1 или 2 tasks[todolistID1]
         // далее фильтруем как обычно
-        setTasks({...tasks, [todolistID]: tasks[todolistID].filter(t => t.id !== taskID)})
+        // setTasks({...tasks, [todolistID]: tasks[todolistID].filter(t => t.id !== taskID)})
         // let filteredTasks = tasks.filter(t => t.id != id);
         // setTasks(filteredTasks);
+        dispatchTasks(removeTaskAC(taskID,todolistID))
     }
 
-    //Функция добавления таски
+    //Функция добавления таски ON REDUCER
     function addTask(todolistID: string, title: string) {
-        let newTask = {id: v1(), title: title, isDone: false};
-        //сетаем таски - копируем массив с тасками ...tasks, далее обращаемся к ключу [todolistID] - кладем туда копию массива [...tasks[todolistID] и добавляем туда newTask
-        setTasks({...tasks, [todolistID]: [...tasks[todolistID], newTask]})
+        // let newTask = {id: v1(), title: title, isDone: false};
+        // //сетаем таски - копируем массив с тасками ...tasks, далее обращаемся к ключу [todolistID] - кладем туда копию массива [...tasks[todolistID] и добавляем туда newTask
+        // setTasks({...tasks, [todolistID]: [...tasks[todolistID], newTask]})
         // let newTasks = [task, ...tasks];
         // setTasks(newTasks);
+        dispatchTasks(addTaskAC(todolistID,title))
     }
 
-    //функция статуса чекбокса
+    //функция статуса чекбокса ON REDUCER
     function changeStatus(todolistID: string, taskId: string, isDone: boolean) {
         //берем таски делаем копию объекта{...tasks, добавляем  ключ [todolistID], и берем tasks[todolistID] таски с ключом,далее  нам надо обновить объект,
         // с помощью .map и обновить статус isDone
-        setTasks({...tasks, [todolistID]: tasks[todolistID].map(el => el.id === taskId ? {...el, isDone} : el)});
+        // setTasks({...tasks, [todolistID]: tasks[todolistID].map(el => el.id === taskId ? {...el, isDone} : el)});
+        dispatchTasks(changeStatusAC(todolistID,taskId,isDone ))
     }
 
-    //Фильтрация тасок
+    // функция изменения названия таски , и которая передаст все в глобальный стейт ON REDUCER
+    const updateTaskTitle = (todolistID: string, taskID: string, newTitle: string) => {
+        // setTasks({
+        //     ...tasks,
+        //     [todolistID]: tasks[todolistID].map(el => el.id === taskID ? {...el, title: newTitle} : el)
+        // })
+        dispatchTasks(updateTaskTitleAC(todolistID,taskID,newTitle))
+    }
+
+
+    //Фильтрация тудулистов
     function changeFilter(todolistID: string, value: FilterValuesType) {
         // объект todolists проходим  с помощью .map и тернарного оператора, по скольку .map и так создает новую КОПИЮ этого объекта
         //{...el, filter:value } это копия нового объекта с копией нового filter:value ключ-значение
@@ -88,22 +102,14 @@ function App() {
     }
 
     const addTodolist = (title: string) => {
-        //для начала генерим айдишки
-        const newTodoListId = v1()
-        //создаем новый тудулист
-        const newTodo: TodoListType = {id: newTodoListId, title, filter: 'all'}
-        //сетаем старый , и добавляем новый
-        setTodolists([...todolists, newTodo])
-        //сетаем объект старых тасок и обращаемся по ключу[newTodoListId]: ( айдишка в качестве свойства) и кладем новый пустой массив(значение)( тасок там нет т.к он новый) [newTodoListId]: []
-        setTasks({...tasks, [newTodoListId]: []})
-    }
-
-    // функция изменения названия таски , и которая передаст все в глобальный стейт
-    const updateTaskTitle = (todolistID: string, taskID: string, newTitle: string) => {
-        setTasks({
-            ...tasks,
-            [todolistID]: tasks[todolistID].map(el => el.id === taskID ? {...el, title: newTitle} : el)
-        })
+        // //для начала генерим айдишки
+        // const newTodoListId = v1()
+        // //создаем новый тудулист
+        // const newTodo: TodoListType = {id: newTodoListId, title, filter: 'all'}
+        // //сетаем старый , и добавляем новый
+        // setTodolists([...todolists, newTodo])
+        // //сетаем объект старых тасок и обращаемся по ключу[newTodoListId]: ( айдишка в качестве свойства) и кладем новый пустой массив(значение)( тасок там нет т.к он новый) [newTodoListId]: []
+        // setTasks({...tasks, [newTodoListId]: []})
     }
 
     // функция изменения названия заголовка Todolist и  которая передаст все в глобальный стейт
@@ -131,7 +137,7 @@ function App() {
                             <Grid item>
                                 <Paper elevation={8} style={{padding: '20px', backgroundColor: '#cdcdcd'}}>
                                     <Todolist
-                                        //сюда передает 9 параметров   key={el.id} его не надо прокидывать дальше т.к ключ указывается как атрибут по умолчанию (для виртуального DOM).
+                                        //сюда передаем 9 параметров  key={el.id} его не надо прокидывать дальше т.к ключ указывается как атрибут по умолчанию (для виртуального DOM).
                                         key={el.id}
                                         todolistID={el.id}
                                         title={el.title}
---
+++ src/Reducer/tasksReducer.ts
@@ -0,0 +1,146 @@
+import {TaskStateType} from "../App";
+import {v1} from "uuid";
+
+export const tasksReducer = (state: TaskStateType, action:tasksReducerType):TaskStateType=> {
+    switch (action.type) {
+        case 'REMOVE-TASK': {
+            // копируем все таски {...tasks , потом взяли ключ [todolistID] , далее тут указываем какие именно таски - 1 или 2 tasks[todolistID1]
+            // далее фильтруем как обычно
+            // setTasks({...tasks, [todolistID]: tasks[todolistID].filter(t => t.id !== taskID)})
+            // let filteredTasks = tasks.filter(t => t.id != id);
+            // setTasks(filteredTasks);
+            return {...state,[action.payload.todolistID]: state[action.payload.todolistID].filter(t => t.id !== action.payload.taskID)}
+        }
+
+        case 'ADD-TASK': {
+            let newTask = {id: v1(), title: action.payload.title, isDone: false};
+            // //сетаем таски - копируем массив с тасками ...tasks, далее обращаемся к ключу [todolistID] - кладем туда копию массива [...tasks[todolistID] и добавляем туда newTask
+            // setTasks({...tasks, [todolistID]: [...tasks[todolistID], newTask]})
+            // let newTasks = [task, ...tasks];
+            // setTasks(newTasks);
+            return {...state, [action.payload.todolistID]: [...state[action.payload.todolistID],newTask ]}
+        }
+
+        case 'CHANGE-STATUS': {
+            return {...state, [action.payload.todolistID]: state[action.payload.todolistID].map(el => el.id === action.payload.taskId ? {...el, isDone: action.payload.isDone}: el)}
+        }
+
+        case 'UPDATE-TASKT-TITLE': {
+            // setTasks({
+            //     ...tasks,
+            //     [todolistID]: tasks[todolistID].map(el => el.id === taskID ? {...el, title: newTitle} : el)
+            // })
+            return {...state,[action.payload.todolistID]: state[action.payload.todolistID].map(el => el.id === action.payload.taskID ?  {...el,title: action.payload.newTitle} : el)}
+        }
+
+        default: return state
+    }
+}
+
+//общая типизация
+export type tasksReducerType = RemoveTaskACType | addTaskACType | changeStatusACType | updateTaskTitleACType
+
+
+//REMOVE TASK
+export type RemoveTaskACType = ReturnType<typeof removeTaskAC>
+export const removeTaskAC = (taskID: string,todolistID:string) => {
+
+    return {
+        type: 'REMOVE-TASK',
+        payload: {taskID: taskID, todolistID}
+    } as const
+}
+
+//ADD TASK
+export type addTaskACType = ReturnType<typeof addTaskAC>
+export const addTaskAC = (todolistID: string, title: string) => {
+
+    return {
+        type: 'ADD-TASK',
+        payload: {title: title, todolistID}
+    } as const
+}
+
+//CHANGE - STATUS
+export type changeStatusACType = ReturnType<typeof changeStatusAC>
+export const changeStatusAC = (todolistID: string, taskId: string, isDone: boolean) => {
+
+    return {
+        type: 'CHANGE-STATUS',
+        payload: {todolistID,taskId,isDone }
+    } as const
+}
+
+//UPDATE-TASKT-TITLE
+export type updateTaskTitleACType = ReturnType<typeof updateTaskTitleAC>
+export const updateTaskTitleAC = (todolistID: string, taskID: string, newTitle: string) => {
+
+    return {
+        type: 'UPDATE-TASKT-TITLE',
+        payload: {todolistID,taskID,newTitle }
+    } as const
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1 создаем обычную функцию например tasksReducer
+// 2 он принимает 2 аргумента - state , action
+// 3 внури Reducer конструкция switch () { case ()}
+// 4 switch - принимает  в параметрах action.type ( тип данных - объект)
+// 5 далее создаем case 'тут указываем ключ'{тут case обязательно что то ретурнит - state, неважно что он что то делает до этого}
+// 6 в конце switch указываем что он возвращает по дефолту - в нашем случае state/
+
+
+// 13 тут протипизируем все наши action - можно собрать их в один - например назоваем его как и называется наша функ
+// tasksReducerType
+
+
+//7 тут пишем функция которая удаляет таски ( например removeTaskAC) и внутри возвращает объект return {}
+//8 внутри return указываем type: тут нужно написать электронный ключ для функции - можно указать например 'REMOVE-TASK'
+//9 типизируем функцию removeTaskAC через typeof
+//10 typeof протипизует всю функцию, таким образом нужно указать что б он протипизировал только объект который ретурнит
+//11 ReturnType <внутри указываем typeof и функцию>
+//12  в конструкции switch у нас может быть много кейсов разных , и в типизации мы могли бы указать тип - string, но тогда
+// у нас все сломается. по этому нам нужно более конкретно сказать - as const - указываем на всем объекте. но можно и на функции
+
+
+// тут пишем функция которая добавляет таски ( например addTaskAC)
+// доделать самому
+
+//14 - далее перейдем в app.tsx закоментим внутренности обоих функций
+//  и всю логику заберем  теперь в tasksReducer
+//15 в функции removeTask addTask есть параметры - id  и title, и нам нужно так же перенести их сюда в наш редьюсер в
+// функции removeTaskAC addTaskAC
+// 16 теперь нам нужен новый ключ в функциях - куда складывать аргументы - например можно назвать payload: {сюда кладем id:id либо другой параметр какой принимала функция}
+
+// 17 - перейти в app tsx и исправить хуки - заменить useState на useReducer а set - заменить на dispatch
+//например let [tasks, dispatchTasks] = useReducer ( принимает 2 аргумента - сам стейт , а 2ой - тот Reducer с которым работает - функцию.)
+
+//18 внутри функций addTask и removeTasks нужно продиспатчить наши функции и передать им внутри id и title.
+
+//19 переходим в switch конструкцию и доделаем наши 2 case - уберем any  и правильно протипизируем state и action
+//20 доделаем case removeTasks - с помощью фильтра
+//21 доделаем case addTasks - с помощью
+
+//23 каждой функции мы должны прикрывать спину типизацией(указать что он вернет)
+// - в Reducer это нужно сделать обязательно - :TaskType[]
